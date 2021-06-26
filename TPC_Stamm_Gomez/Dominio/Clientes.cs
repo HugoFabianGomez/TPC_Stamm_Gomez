fb130553cfb78e9bbaff5ad4ca7dda982c3df778
HugoFabianGomez <hfg.hugo@hotmail.com>
2021-06-26 19:47:09 -0300

    ..

---
+++ TPC_Stamm_Gomez/Dominio/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Clientes
+    {
+        public string cuit { get; set; }
+        public string razonSocial { get; set; }
+        public string contacto { get; set; }
+        public string domicilio { get; set; }
+        public string localidad { get; set; }
+        public string provincia { get; set; }
+        public Clientes (string Cuit, string RazonSocial)
+        {
+            cuit = Cuit;
+            razonSocial = RazonSocial;
+        }
+    }
+
+}
